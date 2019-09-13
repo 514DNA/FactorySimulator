a9bf5455adc9c66e1baeee0b9e4e3fbf826c0aed
514DNA <467085915@qq.com>
2019-09-13 14:16:19 +0800

    Init project with hello world

---
+++ FactorySimulator/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+    }
+}
